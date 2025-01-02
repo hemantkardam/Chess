cac6ae6e0c978e8a010487fff2e39520bc898998
Hemant Kardam <167018529+hemantkardam@users.noreply.github.com>
2025-01-02 18:10:38 +0530

    updated

---
+++ Helper/commonHelper.js
@@ -0,0 +1,200 @@
+import { globalState } from "../index.js";
+import { keySquareMapper } from "../index.js";
+
+// function to check if piece exists of opponent
+function checkPieceOfOpponentOnElement(id, color) {
+  const opponentColor = color === "white" ? "BLACK" : "WHITE";
+
+  const element = keySquareMapper[id];
+
+  if (!element) return false;
+
+  if (element.piece && element.piece.piece_name.includes(opponentColor)) {
+    const el = document.getElementById(id);
+    el.classList.add("captureColor");
+    element.captureHighlight = true;
+    return true;
+  }
+
+  return false;
+}
+
+// function to check weather piece exists or not by square-id
+function checkWeatherPieceExistsOrNot(squareId) {
+  const square = keySquareMapper[squareId];
+
+  if (square.piece) {
+    return square;
+  } else {
+    return false;
+  }
+}
+
+// function to check capture id square
+function checkSquareCaptureId(array) {
+  let returnArray = [];
+
+  for (let index = 0; index < array.length; index++) {
+    const squareId = array[index];
+    const square = keySquareMapper[squareId];
+
+    if (square.piece) {
+      break;
+    }
+    returnArray.push(squareId);
+  }
+
+  return returnArray;
+}
+
+// function to give highlight ids for bishop
+function giveBishopHighlightIds(id) {
+  let finalReturnArray = [];
+
+  // will give top left id
+  function topLeft(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "a" && num != 8) {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) - 1);
+      num = num + 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find bottom left ids
+  function bottomLeft(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "a" && num != 1) {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) - 1);
+      num = num - 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find top right ids
+  function topRight(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "h" && num != 8) {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) + 1);
+      num = num + 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find bottom right ids
+  function bottomRight(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "h" && num != 1) {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) + 1);
+      num = num - 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  return {
+    topLeft: topLeft(id),
+    bottomLeft: bottomLeft(id),
+    topRight: topRight(id),
+    bottomRight: bottomRight(id),
+  };
+}
+
+// function to give highlight ids for rook
+function giveRookHighlightIds(id) {
+  let finalReturnArray = [];
+
+  // will give top left id
+  function top(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (num != 8) {
+      // alpha = String.fromCharCode(alpha.charCodeAt(0) - 1);
+      num = num + 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find bottom left ids
+  function bottom(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (num != 1) {
+      // alpha = String.fromCharCode(alpha.charCodeAt(0) - 1);
+      num = num - 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find top right ids
+  function right(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "h") {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) + 1);
+      // num = num + 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  // find bottom right ids
+  function left(id) {
+    let alpha = id[0];
+    let num = Number(id[1]);
+    let resultArray = [];
+
+    while (alpha != "a") {
+      alpha = String.fromCharCode(alpha.charCodeAt(0) - 1);
+      // num = num + 1;
+      resultArray.push(`${alpha}${num}`);
+    }
+
+    return resultArray;
+  }
+
+  return {
+    top: top(id),
+    bottom: bottom(id),
+    right: right(id),
+    left: left(id),
+  };
+}
+
+export {
+  checkPieceOfOpponentOnElement,
+  checkSquareCaptureId,
+  giveRookHighlightIds,
+  checkWeatherPieceExistsOrNot,
+  giveBishopHighlightIds,
+};
